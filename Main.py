42de06c5d9cacf7444405259129b983eb8b96c1b
Tim Tse <timtse76@gmail.com>
2020-11-19 12:03:48 -0500

    rearranged model organization

--- Main.py
+++ Main.py
@@ -4,11 +4,8 @@ import Utils.TeamAbbreviations as ab
 import sys
 import Models.ModelA as a
 
-
 YEAR = 2020
 WEEKS = 16
-AWAY='ARI'
-HOME='SEA'
 
 def set_options():
     util.set_year(YEAR)
@@ -20,17 +17,7 @@ if __name__ == '__main__':
     elif len(sys.argv) == 3:
         WEEKS = int(sys.argv[1])
         YEAR = int(sys.argv[2])
-    set_options()
-    away_info = util.getSeason(AWAY)
-    home_info = util.getSeason(HOME)
-    results = a.run(away_info, home_info)
-    print(AWAY + ' win: ' + str(results['away']) + '%')
-    print(HOME + ' win: ' + str(results['home']) + '%')
-    print('Tie: ' + str(results['tie']) + '%')
-    print('Avg Score: ' + str(results['away_score']) + ' - ' + str(results['home_score']))
-    print('\n')
-
-    '''
+    set_options() 
     games_file = open('games.txt', 'r')
     matchups = []
 
@@ -46,9 +33,10 @@ if __name__ == '__main__':
     for match in matchups:
         away_info = util.getSeason(match[0])
         home_info = util.getSeason(match[1])
-        results = Models.modelA(away_info, home_info)
+        results = a.run(away_info, home_info)
         print(match[0] + ' win: ' + str(results['away']) + '%')
         print(match[1] + ' win: ' + str(results['home']) + '%')
         print('Tie: ' + str(results['tie']) + '%')
+        print('Avg Score: ' + str(results['away_score']) + ' - ' + str(results['home_score']))
         print('\n')
-    '''
+    
--- Models/ModelA.py
+++ Models/ModelA.py
@@ -1,6 +1,7 @@
 import Utils.StatsUtil as util
 from random import gauss
 
+
 def simGame(away_scores, home_scores):
     away_score = int(round((gauss(away_scores['team'], away_scores['stdev_team']) + gauss(home_scores['opp'], home_scores['stdev_opp']))/2))
     home_score = int(round((gauss(home_scores['team'], home_scores['stdev_team']) + gauss(away_scores['opp'], away_scores['stdev_opp']))/2))
@@ -22,7 +23,6 @@ def simGame(away_scores, home_scores):
             'home': home_score,
             'result': 0
         }
-
 
 def run(away_info, home_info):
     away_scores = util.getScoreStats(away_info)
